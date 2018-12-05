e63b8da33cb857c680e76708ac7e4880c2aa8634
keevbush <keevbush@gmail.com>
2018-12-05 09:41:32 +0200

    [ADD] QuerryImplementation.cs est une classe qui ou l'execution des Requetes Clientes

--- MyCoinServer/Services/QuerryImplementation.cs
+++ MyCoinServer/Services/QuerryImplementation.cs
@@ -8,6 +8,7 @@ using  MyCoinData;
 
 namespace MyCoinServer.Services
 {
+    using MyCoinData.DAO;
     using MyCoinData.IData;
     using MyCoinData.Models;
 
@@ -15,12 +16,48 @@ namespace MyCoinServer.Services
     {
         public string ConnectUser(Querry querry)
         {
-            throw new NotImplementedException();
+            UserDAO userDao = new UserDAO();
+            try
+            {
+                var userActif = (User)querry.Data;
+                var users = userDao.AllItems();
+                var user = users.Where(
+                    u => u.Username.Equals(userActif.Username) && u.Password.Equals(userActif.Password)).First();
+                var response=new Response();
+                user.State = true;
+                response.Data = user;
+                response.Entete = querry.Entete;
+                return new UserDAO().Serialize(response);
+            }
+            catch (Exception e)
+            {
+                var response = new Response();
+                response.Entete = querry.Entete;
+                response.Data = e;
+                return new UserDAO().Serialize(response);
+            }
         }
 
         public string NewUser(Querry querry)
         {
-            throw new NotImplementedException();
+            UserDAO userDao = new UserDAO();
+            try
+            {
+                var userActif = (User)querry.Data;
+                userActif.DateCreation= DateTime.Now;
+                userDao.NewItem(userActif);
+                var response = new Response();
+                response.Data = userActif;
+                response.Entete = querry.Entete;
+                return new UserDAO().Serialize(response);
+            }
+            catch (Exception e)
+            {
+                var response = new Response();
+                response.Entete = querry.Entete;
+                response.Data = e;
+                return new UserDAO().Serialize(response);
+            }
         }
     }
 }
