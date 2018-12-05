26dee293c5e361a1a8745453d38ee21ab9b780dc
keevbush <keevbush@gmail.com>
2018-12-05 09:43:18 +0200

    [ADD] RealTimeServices.cs contient les les thread qui tourneront en arrière plan

---
+++ MyCoinServer/Services/RealTimeServices.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyCoinServer.Services
+{
+    using System.Threading;
+
+    public class RealTimeServices
+    {
+        public static Thread CheckConnexionThread = new Thread(
+            (t) =>
+                {
+
+                });
+        public static Thread CheckInscrptionThread = new Thread(
+            (t) =>
+                {
+
+                });
+    }
+}
