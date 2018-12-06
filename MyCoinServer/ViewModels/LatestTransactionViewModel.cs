fd32c2b3b8d10dc3b06d57edfde69e8b34eed32f
keevbush <keevbush@gmail.com>
2018-12-06 21:16:16 +0200

    [ADD] ViewModel dor UserListTransaction meme si le nom n'est pas pareille a modifier prochainement

---
+++ MyCoinServer/ViewModels/LatestTransactionViewModel.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyCoinServer.ViewModels
+{
+    using System.Collections.ObjectModel;
+    using System.ComponentModel;
+    using System.Windows;
+
+    using MyCoinData.Models;
+
+    public class LatestTransactionViewModel:INotifyPropertyChanged
+    {
+        public ObservableCollection<User> LstActifUsers { get; set;}
+
+        public DelegateCommand AddUserCommand { get; set; }
+
+        public DelegateCommand DeleteUserCommand { get; set; }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        public LatestTransactionViewModel()
+        {
+            LstActifUsers= new ObservableCollection<User>
+                               {
+                                   new User()
+                                       {
+                                           DateCreation = DateTime.Now
+                                       },
+                                   new User()
+                                       {
+                                           DateCreation = DateTime.Today
+                                       },
+                                   new User()
+
+                               };
+            this.AddUserCommand=new DelegateCommand(ExecuteAddUserCommand);
+        }
+
+        private bool CanExecuteAddUserCommand()
+        {
+            return true;
+        }
+
+        private void ExecuteAddUserCommand(object obj)
+        {
+
+            MessageBox.Show($"{obj}");
+        }
+    }
+}
