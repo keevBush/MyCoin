cfb5670913f6901df60fd7c769ffab6669dcd506
keevbush <keevbush@gmail.com>
2018-12-06 21:15:17 +0200

    [ADD] DelegateCommand.cs the event for all mvvm bind commad

---
+++ MyCoinServer/ViewModels/DelegateCommand.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyCoinServer.ViewModels
+{
+    using System.Windows.Input;
+
+    public class DelegateCommand : ICommand
+    {
+        private Action<object> Action;
+
+        private Func<bool> IfCanExecute;
+
+        public DelegateCommand(Action<object> action)
+        {
+            this.Action = action;
+            //this.IfCanExecute = canExecute;
+        }
+        public event EventHandler CanExecuteChanged;
+
+        public bool CanExecute(object parameter)
+        {
+            return true;
+        }
+
+        public void Execute(object parameter)
+        {
+            this.Action(parameter);
+        }
+    }
+}
