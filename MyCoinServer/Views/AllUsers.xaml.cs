f74bbbee10f7d7bcb7ed1c1c9b3c194d7def1ebd
keevbush <keevbush@gmail.com>
2018-12-07 11:42:54 +0200

    [ADD] AllUser.xam and .cs ajout de la vue qui controle les users

---
+++ MyCoinServer/Views/AllUsers.xaml.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace MyCoinServer.Views
+{
+    using MyCoinServer.ViewModels;
+
+    /// <summary>
+    /// Interaction logic for AllUsers.xaml
+    /// </summary>
+    public partial class AllUsers : UserControl
+    {
+        public AllUsers()
+        {
+            InitializeComponent();
+            DataContext = new AllUserViewModel();
+        }
+    }
+}
